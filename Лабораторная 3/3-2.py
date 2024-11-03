7ad0b38bb64114c04b2eeb171488700e20e53ad8
klublubiteley <150192216+klublubiteley@users.noreply.github.com>
2024-11-03 21:24:20 +0300

    Лабораторная 3

---
+++ Лабораторная 3/3-2.py
@@ -0,0 +1,8 @@
+def find_common_participants(group_1, group_2, separator=','):
+    return list(set(group_1.split(separator)).intersection(set(group_2.split(separator))))
+
+
+participants_first_group = "Иванов|Петров|Сидоров"
+participants_second_group = "Петров|Сидоров|Смирнов"
+
+print(find_common_participants(participants_first_group, participants_second_group, '|'))
